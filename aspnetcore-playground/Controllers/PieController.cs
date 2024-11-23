89347fa1e2bca29809c631dc4ff9c365cc454db0
HudorHasHasHa <jankowskiigor5@gmail.com>
2024-11-23 17:17:40 +0100

    add basic controller

---
+++ aspnetcore-playground/Controllers/PieController.cs
@@ -0,0 +1,22 @@
+﻿using aspnetcore_playground.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore_playground.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(ICategoryRepository categoryRepository, IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
